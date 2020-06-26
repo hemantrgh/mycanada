06176305020bf8a9ecf7b16113020f0e0191e0ba
Hemant <hemant.ghodke86@gmail.com>
2020-06-26 20:45:41 +0530

    first commit

---
+++ app/src/main/java/com/mycanada/poc/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.mycanada.poc
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.mycanada.poc.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
